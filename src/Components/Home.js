1db1b006c056d47c82d8314086b6612acf402ff2
EricaRose1 <115095732+EricaRose1@users.noreply.github.com>
2023-11-27 17:26:26 -0500

    updated design

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import '../Styles/Home.css';
-import background from '../Styles/photos/design2.png';
-import extra from '../Styles/photos/designCircles.png';
+import background from '../Styles/photos/design1127.png';
 
-// import Projects from './Projects';
 
 function Home() {
     return (
@@ -12,6 +10,7 @@ function Home() {
                 <img className='background' src={background}/>
                 <h1 className='title'>Erica Litzenberger - Web Developer</h1>
                 <p className='catchphrase'>Passionate Web Developer Crafting Digital Experiences.</p>
+            
             </div>
         </div>
     )
